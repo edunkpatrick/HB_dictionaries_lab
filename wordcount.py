29d65127ddbb1a566beb259935fa2feb40ff4bb4
Erin Dunkley <eskilpatrick@gmail.com>
2022-12-19 12:25:59 -0800

    Initial commit with pseudocode

---
+++ wordcount.py
@@ -0,0 +1,22 @@
+"""Count words in file."""
+
+def word_count(file):
+
+    #inputs: given file full of words 
+    #output: a printed accounting of how many times each word occurs within the file
+
+    # open the text file in variable
+
+    # create empty dictionary container held by a meaningfully named variable that can be returned
+
+    # iterate over words in file to access each word
+        
+        # for letter in phrase:
+        # letter_counts[letter] = letter_counts.get(letter, 0) + 1
+
+        # for word in phrase:
+        # delimiter is space between words -(" ")
+        # dictionary[word] = dictionary.get(word, 0) + 1
+
+    # print dictionary
+
